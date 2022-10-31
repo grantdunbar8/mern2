7c8322a04054d9a93479620c3667ba8fd1d3dd57
Grant Dunbar <grantdunbar8@gmail.com>
2022-10-31 16:52:25 -0600

    Update MyGithub.jsx

--- my-app/src/components/MyGithub.jsx
+++ my-app/src/components/MyGithub.jsx
@@ -1,6 +1,6 @@
 const MyGithub = () => {
     return(
-        <p>Link to GitHub: <a href=''></a></p>
+        <p>Link to GitHub: <a href='https://github.com/grantdunbar8/mern2'>https://github.com/grantdunbar8/mern2</a></p>
     )
 }
 
